6d42199c11ab5e904aa80646b85e120bc320de36
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-23 16:12:07 +0530

    SoundaryaD_main10

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from UtilTestcases import test_number_to_pair,test_pair_to_number 
 from ConsoleOutputDisplay import display_output
-from ColorGuide import build_color_guide
+from ColorReference import build_color_reference
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
@@ -9,5 +9,5 @@ if __name__ == '__main__':
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
   display_output('Done :)')
-  manual=build_color_guide()
+  manual=build_color_reference()
   display_output(manual)
