223b6e856b3dc1b8a97a4e6e09da2f0d1c129db4
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-23 16:11:16 +0530

    SoundaryaD_ColorReference

--- ColorReference.py
+++ ColorReference.py
@@ -1,5 +1,5 @@
 from ColorUtils import get_color_from_pair_number, MAJOR_COLORS, MINOR_COLORS
-def build_color_guide():
+def build_color_reference():
     manual = "Color Coding Reference:\n"
     for pair_number in range(1, len(MAJOR_COLORS) * len(MINOR_COLORS) + 1):
         major_color, minor_color = get_color_from_pair_number(pair_number)
