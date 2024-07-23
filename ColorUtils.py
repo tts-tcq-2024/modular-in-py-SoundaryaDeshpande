9090afba1ff23279be3f5f145f55d307454e3698
SoundaryaDeshpande <98644073+SoundaryaDeshpande@users.noreply.github.com>
2024-07-23 15:38:50 +0530

    ColorUtils1

--- ColorUtils.py
+++ ColorUtils.py
@@ -22,9 +22,3 @@ def get_pair_number_from_color(major_color, minor_color):
         raise Exception('Minor index out of range')
     return major_index * len(MINOR_COLORS) + minor_index + 1
 
-def build_color_guide():
-    manual = "Color Coding Reference:\n"
-    for pair_number in range(1, len(MAJOR_COLORS) * len(MINOR_COLORS) + 1):
-        major_color, minor_color = get_color_from_pair_number(pair_number)
-        manual += f"Pair Number {pair_number}: {major_color} {minor_color}\n"
-    return manual
